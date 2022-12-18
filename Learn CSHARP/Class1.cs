38faee1d07396a1df12e4998bdaf95ef78af2396
Dmitry <d3kon@mail.ru>
2022-12-18 21:23:17 +0800

    Операции над числами

--- Learn CSHARP/Class1.cs
+++ Learn CSHARP/Class1.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Learn_CSHARP
 {
-    internal class Class1
+    public static class LearnCSHARP
     {
+        public static void Sum()
+        {
+            int x = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sum = {0}", x+z);
+        }
+        public static void Subtraction()
+        {
+            float x = float.Parse(Console.ReadLine());
+            float z = float.Parse(Console.ReadLine());
+            Console.WriteLine("Sum = {0}", x-z);
+        }
+        public static void Multiplication()
+        {
+            float x = float.Parse(Console.ReadLine());
+            float z = float.Parse(Console.ReadLine());
+            Console.WriteLine("Sum = {0}", x*z);
+        }
+        public static void Division()
+        {
+            float x = float.Parse(Console.ReadLine());
+            float z = float.Parse(Console.ReadLine());
+            Console.WriteLine("Sum = {0}", x/z);
+        }
+
     }
+    
 }
--- Learn CSHARP/Program.cs
+++ Learn CSHARP/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Learn_CSHARP;
+
+
+Console.WriteLine("Сумма");
+LearnCSHARP.Sum();
+Console.WriteLine("Вычитание");
+LearnCSHARP.Subtraction();
+Console.WriteLine("Умножение");
+LearnCSHARP.Multiplication();
+Console.WriteLine("Умножение");
+LearnCSHARP.Division();
